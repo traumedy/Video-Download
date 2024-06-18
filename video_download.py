c8e370d1b051f944e97658560d2f2c70e6061c2a
Sir Dystic <4115891-sirdystic@users.noreply.gitlab.com>
2024-06-18 01:25:05 -0700

    Increment minor version number

--- video_download.py
+++ video_download.py
@@ -9,7 +9,7 @@ Author: Josh Buchbinder
 
 __author__ = "Josh Buchbinder"
 __copyright__ = "Copyright 2024, Josh Buchbinder"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 import sys
 import shutil
